7a664b8a84318b784a99b0106894b8956c302b4e
HobumPark <gloryofalive@naver.com>
2023-02-01 02:26:15 +0900

    config set

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
 	app.use(
-		createProxyMiddleware('/v1/search/book.json?query=*', {
+		createProxyMiddleware('/v1/search/*', {
 			target: 'https://openapi.naver.com', 
 			changeOrigin: true,
 		})
